ac8309a52fe85fe58a22b7723e3735dfe11a5fe2
JOSE MARIA GARCIA DAROCA <110504961+JOSEDAROCA@users.noreply.github.com>
2022-08-04 23:10:14 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>EJERCICIO CONSOLE !</h1>
+<div>
+  Utilizando 10 lineas de console.log
+  
+</div>`;
+
+console.log("Pasos para cocinar una Torta");
+console.log("Elegir la Receta");
+console.log("Juntar todos los componentes de la Receta");
+console.log("Preparar los utensillos necesarios");
+console.log("Seguir el Paso a Paso de la Receta");
+console.log("Precalntar el Horno");
+console.log("Verter la rceta en el molde");
+console.log("Meter al horno por el tiempo indicado a la temperatura indicda");
+console.log(
+  "Verificar con un palillo de Madera si al pincharla sale seco y retirar del horno"
+);
